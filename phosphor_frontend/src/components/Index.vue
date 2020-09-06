aa6769da76586dbfd56339166260be6c1d468230
chenqiufan <1334607406@qq.com>
2020-09-06 10:37:45 +0800

    chmod ip

---
+++ phosphor_frontend/src/components/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Hello , Zhu Jian</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "Index"
+}
+</script>
---
+++ phosphor_frontend/src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p>
+            <label for="username">用户名:</label>
+            <input type="text" id="username">
+        </p>
+        <p>
+            <label for="pwd">密码:</label>
+            <input type="password" id="pwd">
+        </p>
+        <p>
+            <router-link to="Index"><button v-on="LogIn()">登录</button></router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name : "Login",
+    methods: {
+        LogIn(){
+            const data = {
+                "name":"zhu jian"
+            }
+            axios.post('http://127.0.0.1:59003/login',data)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(function (error){
+                console.log(error)
+            })
+        }
+    },
+}
+</script>
